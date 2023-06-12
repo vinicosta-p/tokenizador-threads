9e721a8f73d6e3c1268ce5a5c88b7bec4e425a65
Vinícius Costa <84977966+vinicosta-p@users.noreply.github.com>
2023-06-12 16:45:39 -0300

    Arquivos finais que foram entregues

--- Códigos/threads.cpp
+++ Códigos/threads.cpp
@@ -1,145 +1,261 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <sstream>
+#include <chrono>
+#include <mutex> 
+#include <thread>
 #include <map>
-#include <vector>
-#include <algorithm>
-#include <typeinfo>
+#include <sstream>  // for string streams
+#include <string>  // for string
 
 using namespace std;
+fstream DATASET_CSV, DATASET_FINAL;
+// COLUNAS COM DADOS CARTEGORICAS: 2, 3, 4, 6, 7, 8, 9, 17, 18, 20, 23
+// cada var representa o nome de uma coluna
+fstream cdtup,	berco,	portoatracacao,	mes, tipooperacao,	tiponavegacaoatracacao,	terminal,
+nacionalidadearmador, origem, destino, naturezacarga, sentido;
 
-vector<pair<int,string>> ordenaDict(map<int,string> id_dict){
-    vector<pair<int,string>> id_vec_pair;
+// BUFFER 
+const int BUFFER_SIZE = 101;
+const int BUFFER_COL = 26;
+string buffer_de_escrita[BUFFER_SIZE][BUFFER_COL];
+string palavra_excedente;
 
-    copy(id_dict.begin(), id_dict.end(), back_inserter<vector<pair<int,string>>>(id_vec_pair));
+mutex mtx_buffer; // para o buffer 
+mutex mtx_final; // para o arquivo final
+mutex mtx_conditional_escrita;
+mutex mtx_leitura_do_buffer;
+//Definem o fim das interações
+int interacoes_internas = 0;
+int interacoes_externas = 0;
+bool EVENT_BREAK_LINE = false;
+//Define se a linha já pode ser escrita
+const int QNTD_DE_THREADS = 6;
+bool flag_escreva[BUFFER_SIZE][QNTD_DE_THREADS] = {false};
 
-    sort(id_vec_pair.begin(), id_vec_pair.end(), [](const pair<int,string> &l, const pair<int,string> &r){
-        if (l.first != r.first){
-            return l.first < r.first;
-        }
-
-        return l.second < r.second;
-    });
-
-    return id_vec_pair;
-}
-
-bool verificaElemento(string elemento, map<int,string> id_dict){
-	for (auto const &i : id_dict){
-		if (i.second == elemento){
-			return true;
-		}
-	}
-	return false;
-}
-
-int main ()
-{
-    map<int,string> id_dict;
-    int id = 1;
-
-    ifstream arquivo("test_date.csv");
+bool inicializou_a_primeira_linha = false; // variavel apenas para ler uma unica vez
+void leitura_do_arquivo(){
+    if(inicializou_a_primeira_linha == false){
+        mtx_final.lock();
+        DATASET_CSV.open("dataset_00_1000_sem_virg.csv", ios::in);
+        DATASET_FINAL.open("dataset_final.csv",ios::out);
+        // escreve a primeira linha no csv
+        string primeira_linha;
+        getline(DATASET_CSV, primeira_linha);
+        DATASET_FINAL << primeira_linha.c_str() << endl;
+        mtx_final.unlock();
+        
+        inicializou_a_primeira_linha = true;
+    }
     string linha;
+    char delim = ',';
+    /*
+    A intenção no código pe que ele tenha a seguinte etapa
 
-    while (getline(arquivo, linha))
-    {
-        stringstream linhadoc(linha);
-        string elemento;
-        while (getline(linhadoc, elemento, ','))
-        {
-			if(verificaElemento(elemento, id_dict)){
-				continue;
-			}
-            else{
-				id_dict.insert({id, elemento});
-            	id++;
-			}
+    uma thread lerá o arquivo inicial e escreverá em um buffer,
+    ao fim da escrita do buffer a thread deve ficar "fechada" enquanto 
+    as outras são liberadas para tratar o buffer. Houveram muitas tentativas
+    com mutex, whiles, até hibernate mas nenhuma deu certo.
+
+    esta thread tbm define o fim das outras
+    quando ela acaba ela avisa através de uma variavel EVENT_BREAK_LINE
+    e as threads começam a verificar se já é para acbar
+    */
+    if (DATASET_CSV.is_open()){
+        int i = 0;
+        int j = 0;
+        int teste_de_interacao = 0;
+        while(getline(DATASET_CSV, linha, delim)){
+            if(i == BUFFER_SIZE){
+                interacoes_externas++;
+                std::this_thread::sleep_for(std::chrono::milliseconds(3));
+                i = 0;
+                
+            }
+                
+           
+            mtx_buffer.lock();
+            
+            if(j == 25){
+                buffer_de_escrita[i][j] = linha.substr(0, linha.find("\n"));
+                if (i == BUFFER_SIZE-1){
+                    palavra_excedente = linha.substr(linha.find('\n'), linha.length());
+                } else {
+                    buffer_de_escrita[i+1][0] = linha.substr(linha.find('\n'), linha.length());
+                }
+            } else {
+                buffer_de_escrita[i][j] = linha;
+            }
+            mtx_leitura_do_buffer.lock();
+            flag_escreva[i][0] = true;
+            mtx_leitura_do_buffer.unlock();
+            mtx_buffer.unlock();
+            
+            linha.clear();
+            j++;
+            if(j == 26){
+                i++;
+                j = 1;
+            }
+            
         }
+        interacoes_internas = i;
+        EVENT_BREAK_LINE = true;
+        DATASET_CSV.close();
     }
-
-	arquivo.close();
     
-    vector<pair<int,string>> id_vector_dict = ordenaDict(id_dict);
+}
+void escrita_do_arquivo(){
+    int i = 0;
+    int idx_interacoes = 0;
+    while(true){
 
-	/*
-    for (auto const &pair: id_vector_dict){
-        cout << pair.first << " : " << pair.second << endl;
-    }
-	*/
+        while(i != BUFFER_SIZE){
 
-	//Cria arquivo que está o dicionário de ids
-	ofstream arquivo_dic;
-
-    arquivo_dic.open("dict.txt");
-
-    for (auto const &i: id_vector_dict){
-        arquivo_dic << i.first << "," << i.second << endl;
-    }
-
-	arquivo_dic.close();
-
-
-	//Lê o arquivo do dicionáiro e o constroi em um novo
-	ifstream arquivo_di("dict.txt", ios::in);
-    string ln;
-	string elem;
-	map <int, string> new_id_dict;
-
-    while(getline(arquivo_di, ln)){
-		//cout << ln << endl;
-		stringstream l(ln);
-
-		vector <string> elem_ln;
-
-		while(getline(l, elem, ',')){
-			elem_ln.push_back(elem);
-		}
-
-		new_id_dict.insert({stol(elem_ln[0]), elem_ln[1]});
-    }
-
-	arquivo_di.close();
-
-
-	vector<pair<int,string>> new_id_vector_dict = ordenaDict(new_id_dict);
-
-    for (auto &i : new_id_vector_dict){
-        cout << i.first << " : " << i.second << endl;
-    }
-
-
-	//escrevendo em um arquivo a tabela só com os ids
-
-	//arquivo para ler 
-	ifstream arq_original("test_date.csv", ios::in);
-
-	//arquivo para escrever
-	ofstream arq_id;
-	arq_id.open("id_tabela.txt");
-
-    string ln1;
-	string elem1;
-
-    while(getline(arq_original, ln1)){
-		//cout << ln << endl;
-		stringstream l(ln1);
-		vector <string> elem_ln;
-
-		while(getline(l, elem1, ',')){
-			for (auto const &i: id_vector_dict){
-				if (elem1 == i.second){
-					arq_id << i.first << ",";
-				}
-    		}
-		}
-		arq_id << endl;
-    }
+            if(EVENT_BREAK_LINE == true){ 
+                if(idx_interacoes >= interacoes_externas && interacoes_internas-1 <= i){
+                    break;
+                }
+            }
+            //flag que verifica se os processos já acabaram na linha i se sim ele escreve no arquivo
+            if(flag_escreva[i][0] == true && flag_escreva[i][1] == true && flag_escreva[i][2] == true && 
+                flag_escreva[i][3] == true && flag_escreva[i][4] == true && flag_escreva[i][5] == true){
+                mtx_final.lock();
+                mtx_buffer.lock();
+                for(int j = 0; j < BUFFER_COL; j++){
+                    DATASET_FINAL << buffer_de_escrita[i][j].c_str() << ","; 
+                }
+                mtx_leitura_do_buffer.lock();
+                for(int idx_flag = 0; idx_flag < QNTD_DE_THREADS; idx_flag++){
+                    flag_escreva[i][idx_flag] = false;
+                   // cout << "f"<< idx_flag << endl;
+                }
+                mtx_leitura_do_buffer.unlock();
+                mtx_buffer.unlock();
+                mtx_final.unlock();
+                if(i == 100){
+                    std::this_thread::sleep_for(std::chrono::milliseconds(3));
+                }
+                i++;
+            }       
+        }
 
     
-	arq_id.close();
-	arq_original.close();
+        //cout << idx_interacoes << " " << i << endl;
+        if(EVENT_BREAK_LINE == true){ 
+            if(idx_interacoes >= interacoes_externas &&  i >= interacoes_internas-1){
+            //cout << "FIM: " << interacoes_externas << " " << interacoes_internas << endl;
+            break;
+            }
+        }
+        idx_interacoes++;
+        i = 0;
+    }
+}
 
+void escreve_id(int coluna, int id_da_thread, int col_2, int id_thread_2, bool dois_valores){
+        
+        int idx_mudança = coluna;
+        int idx_interacoes = 0;
+        fstream arquivo;
+        bool existe_no_dic = false;
+        int i = 0;
+        int id = 1;
+        int id_2 = 1;
+        map<string, int> dicionario;
+        map<string, int> dicionario2;
+        std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        while (true)
+        {   
+       
+        while (i != BUFFER_SIZE) {
+                    
+            std::this_thread::sleep_for(std::chrono::milliseconds(3));
+                    
+            if(EVENT_BREAK_LINE == true){ 
+                if(idx_interacoes >= interacoes_externas &&  i >= interacoes_internas-1 ){
+                        break;
+                    }
+            }
+                    
+                mtx_buffer.lock();
+                string pesquisa_no_dicionario = buffer_de_escrita[i][idx_mudança];
+                pesquisa_no_dicionario.append(",");
+                if(dicionario[pesquisa_no_dicionario] == false || dicionario.empty()){
+                    dicionario[pesquisa_no_dicionario] = id;
+                    id++;
+                }
+                // tranforma o int
+                ostringstream aux;
+                aux << dicionario[pesquisa_no_dicionario];
+                string id_char = aux.str();
 
-    return EXIT_SUCCESS;
+                    
+                buffer_de_escrita[i][idx_mudança] = id_char;
+                   
+                //repete o que foi feito                   
+                if(dois_valores == true){
+                        
+                    string pesquisa_no_dicionario_2 = buffer_de_escrita[i][col_2];
+                    pesquisa_no_dicionario_2.append(",");
+                    if(dicionario2[pesquisa_no_dicionario_2] == false || dicionario2.empty()){
+                        dicionario2[pesquisa_no_dicionario_2] = id_2;
+                        id_2++;
+                    }
+                    ostringstream aux_2;
+                    aux_2 << dicionario2[pesquisa_no_dicionario_2];
+                    string id_char_2 = aux_2.str();
+                    buffer_de_escrita[i][col_2] = id_char_2;
+
+                }
+                    
+                    mtx_leitura_do_buffer.lock();
+                    flag_escreva[i][id_da_thread] = true;
+                    mtx_leitura_do_buffer.unlock();
+                    
+                    mtx_buffer.unlock();
+                    
+                    i++;
+                    
+                    pesquisa_no_dicionario.clear(); 
+
+                    }
+                
+        
+                    if(EVENT_BREAK_LINE == true){ 
+                        if(idx_interacoes >= interacoes_externas &&  i >= interacoes_internas-1){
+                            break;
+                        }
+                    }
+              
+
+            
+            idx_interacoes++;
+            i = 0;
+        }       
+}
+
+int main(){
+    auto start = std::chrono::steady_clock::now();
+    
+    thread TH1(leitura_do_arquivo);
+    thread TH2(escreve_id, 1, 1, 17, 7, true);
+    thread TH3(escreve_id, 2, 2, 18, 8, true);
+    thread TH4(escreve_id, 3, 3, 20, 9, true);
+    thread TH5(escreve_id, 5, 4, 23, 10, true);
+    thread TH6(escreve_id, 8, 5, 6, 11, true);
+    thread TH7(escreve_id, 7, 6, 0, 0, false);
+    thread TH8(escrita_do_arquivo);
+
+    TH1.join();
+    TH2.join();
+    TH3.join();
+    TH4.join();
+    TH5.join();
+    TH6.join();
+    TH7.join();
+    TH8.join();
+
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "Tempo: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+    return 0;
 }
--- Códigos/threads_id.cpp
+++ Códigos/threads_id.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 #include <fstream>
-#include <omp.h>
+#include <chrono>
+#include <mutex> 
+#include <thread>
+#include <map>
+#include <sstream>  // for string streams
+#include <string>  // for string
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+    mutex bloqueio;
+    bool last = false;
+
     ifstream DATASET_CSV;
     // COLUNAS COM DADOS CARTEGORICAS: 2, 3, 4, 6, 7, 8, 9, 17, 18, 20, 23
     // cada var representa o nome de uma coluna
     // possível melhoria: trabalhar com vetores de ifstream para tornar dinamico
     // e verificar quais colunas são categóricas
-    ofstream cdtup,	berco,	portoatracacao,	mes, tipooperacao,	tiponavegacaoatracacao,	terminal,
+    fstream cdtup,	berco,	portoatracacao,	mes, tipooperacao,	tiponavegacaoatracacao,	terminal,
     nacionalidadearmador, origem, destino, naturezacarga, sentido;
-;
 
-    DATASET_CSV.open("dataset_00_1000_sem_virg.csv", ios::in);
+void principal(){
+    bloqueio.lock();
     char delim = ',';
+    DATASET_CSV.open("dataset_00_1000_sem_virg.csv", ios::in);
+ 
 
     //Inicialização das colunas
-    cdtup.open("cdtup.csv");	
-    berco.open("berco.csv");	
-    portoatracacao.open("portoatracacao.csv");
-    mes.open("mes.csv");
-    tipooperacao.open("tipooperacao.csv");
-    tiponavegacaoatracacao.open("tiponavegacaoatracacao.csv");
-    terminal.open("terminal.csv");
-    origem.open("origemcsv");
-    destino.open("destino.csv");
-    naturezacarga.open("naturezacargacsv");
-    sentido.open("sentido.csv");
+    cdtup.open("cdtup.csv", ios::out);	
+    berco.open("berco.csv", ios::out);	
+    portoatracacao.open("portoatracacao.csv", ios::out);
+    mes.open("mes.csv", ios::out);
+    tipooperacao.open("tipooperacao.csv", ios::out);
+    tiponavegacaoatracacao.open("tiponavegacaoatracacao.csv", ios::out);
+    terminal.open("terminal.csv", ios::out);
+    origem.open("origem.csv", ios::out);
+    destino.open("destino.csv", ios::out);
+    naturezacarga.open("naturezacarga.csv", ios::out);
+    sentido.open("sentido.csv", ios::out);
 
 
 
@@ -54,8 +63,6 @@ int main(int argc, char *argv[]) {
         // fim da coluna
         if (coluna == 25){
             coluna = 0;
-            count++;
-            cout << count << endl;
             continue;
         }
 
@@ -83,16 +90,16 @@ int main(int argc, char *argv[]) {
         case 9:
             terminal << linha << endl;
             break;
-        case 17:
+        case 18:
             origem << linha << endl;
             break;
-        case 18:
+        case 19:
             destino << linha << endl;
             break;
-        case 20:
+        case 21:
             naturezacarga << linha << endl;
             break;
-        case 21:
+        case 24:
             sentido << linha << endl;
             break;
         default:
@@ -100,12 +107,304 @@ int main(int argc, char *argv[]) {
         }
         }
 
-    } else {
-        return -1;
     }
     DATASET_CSV.close();
+    bloqueio.unlock();
+}
+void escreve_id(fstream &arquivo);
+
+void principal_final(){
+     while (true)
+    {
+        if(bloqueio.try_lock() and last){
+            break;
+        }
+    }
+    cdtup.open("cdtup_id.csv", ios::in);	
+    berco.open("berco_id.csv", ios::in);	
+    portoatracacao.open("portoatracacao_id.csv", ios::in);
+    mes.open("mes_id.csv", ios::in);
+    tipooperacao.open("tipooperacao_id.csv", ios::in);
+    tiponavegacaoatracacao.open("tiponavegacaoatracacao_id.csv", ios::in);
+    terminal.open("terminal_id.csv", ios::in);
+    origem.open("origem_id.csv", ios::in);
+    destino.open("destino_id.csv", ios::in);
+    naturezacarga.open("naturezacarga_id.csv", ios::in);
+    sentido.open("sentido_id.csv", ios::in);
+
+    ofstream DATASET_FINAL;
+    DATASET_FINAL.open("dataset_com_ids.csv", ios::app);
+
+    DATASET_CSV.open("dataset_00_1000_sem_virg.csv", ios::in);
+    DATASET_CSV.seekg(ios::beg);
+    char delim = ',';
+    string linha;
+    string dado;
+    bool primeira_leitura = true;
+    int coluna = 0;
+    getline(cdtup, dado);
+ 
+    getline(berco, dado);
+      
+    getline(portoatracacao, dado);
+    
+    getline(mes, dado);
+       
+    getline(tipooperacao, dado);
+      
+    getline(tiponavegacaoatracacao, dado);
+
+    getline(terminal, dado);
+
+    getline(origem, dado);
+ 
+    getline(destino, dado);
+
+    getline(naturezacarga, dado);
+
+    getline(sentido, dado);
+    linha.clear();
+    dado.clear();
+    if (DATASET_CSV.is_open()){
+        while(getline(DATASET_CSV, linha, delim)){
+        coluna++;
+        // fim da coluna
+        if (coluna == 25){
+            coluna = 0;
+            DATASET_FINAL << linha.append(",");
+            dado.clear();
+            linha.clear();
+            primeira_leitura = false;
+            continue;
+        }
+
+        if(primeira_leitura){
+            DATASET_FINAL << linha.append(",");
+            continue;
+        }
+
+        //if(coluna != 2, 3, 4, 6, 7, 8, 9, 17, 18, 20, 23)
+
+        switch (coluna){
+        case 2:
+            getline(cdtup, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 3:
+            getline(berco, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 4:
+            getline(portoatracacao, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 6:
+            getline(mes, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 7:
+            getline(tipooperacao, dado);
+            DATASET_FINAL << dado;
+            break;    
+        case 8:     
+            getline(tiponavegacaoatracacao, dado);
+            DATASET_FINAL << dado;                
+            break;
+        case 9:
+            getline(terminal, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 18:
+            getline(origem, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 19:
+            getline(destino, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 21:
+            getline(naturezacarga, dado);
+            DATASET_FINAL << dado;
+            break;
+        case 24:
+            getline(sentido, dado);
+            DATASET_FINAL << dado;
+            break;
+        default:
+            DATASET_FINAL << linha.append(",");
+            break;
+        dado.clear();
+        linha.clear();
+        }
+        }
+    }
+    bloqueio.unlock();
+    cdtup.close();
+    berco.close();	
+    portoatracacao.close();
+    mes.close();
+    tipooperacao.close();
+    tiponavegacaoatracacao.close();
+    terminal.close();
+    origem.close();
+    destino.close();
+    naturezacarga.close();
+    sentido.close();
+    remove("cdtup_id.csv");
+    remove("berco_id.csv");
+    remove("portoatracacao_id.csv");
+    remove("mes_id.csv");
+    remove("tipooperacao_id.csv");
+    remove("tiponavegacaoatracacao_id.csv");
+    remove("terminal_id.csv");
+    remove("origem_id.csv");
+    remove("destino_id.csv");
+    remove("naturezacarga_id.csv");
+    remove("cdtup_id.csv");
+    remove("sentido_id.csv");
+
+}
+
+void funcao_do_meio(){
+    while (true)
+    {
+        if(bloqueio.try_lock()){
+            break;
+        }
+    }
+    cdtup.close();
+    cdtup.open("cdtup.csv", ios::in);	
+    escreve_id(cdtup);
+    
+    berco.close();
+    berco.open("berco.csv", ios::in);	
+    escreve_id(berco);
+    
+    portoatracacao.close();
+    portoatracacao.open("portoatracacao.csv", ios::in);	
+    escreve_id(portoatracacao);
+
+    mes.close();
+    mes.open("mes.csv", ios::in);
+    escreve_id(mes);
+    
+    tipooperacao.close();
+    tipooperacao.open("tipooperacao.csv", ios::in);
+    escreve_id(tipooperacao);
+    
+    tiponavegacaoatracacao.close();
+    tiponavegacaoatracacao.open("tiponavegacaoatracacao.csv", ios::in);
+    escreve_id(tiponavegacaoatracacao);
+
+    terminal.close();
+    terminal.open("terminal.csv", ios::in);
+    escreve_id(terminal);
+    
+    origem.close();
+    origem.open("origem.csv", ios::in);   
+    escreve_id(origem);
+
+    destino.close();
+    destino.open("destino.csv", ios::in);
+    escreve_id(destino);
+    
+    naturezacarga.close();
+    naturezacarga.open("naturezacarga.csv", ios::in);
+    escreve_id(naturezacarga);
+    
+    sentido.close();
+    sentido.open("sentido.csv", ios::in);    
+    escreve_id(sentido);
+    last = true;
+    bloqueio.unlock();
+
+}
+int main() {
+    auto start = std::chrono::steady_clock::now();
+    thread th1(principal);
+    thread th2(funcao_do_meio);
+    thread th3(principal_final);
+    th1.join();
+    th2.join();
+    th3.join();
+
+
+
     // FIM DA LEITURA E ESCRITA DE ARQUIVOS
-    //Teste
+    
     // FUNC 2: PARALELIZANDO A IDENTIFICAÇÃO DE ID E ESCRITA DE ARQUIVO
+    
+    /*
+    thread th1(escreve_id,cdtup);
+    th1.join();
+    thread th2(escreve_id,berco);
+    th2.join();
+    thread th3(escreve_id,portoatracacao);
+    th3.join();
+    thread th4(escreve_id,mes);
+    th4.join();
+    thread th5(escreve_id,tipooperacao);
+    th5.join();
+    thread th6(escreve_id,tiponavegacaoatracacao);
+    th6.join();
+    */
+    //FUNC 3: ESCREVENDO NO ARQUIVO
+    
+    
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "Tempo       : " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
     return 0;
+}
+
+void escreve_id(std::fstream &arquivo){
+    string linha;
+    ofstream ID_Arquivo;
+    map<string, int> dicionario;
+    int id = 1; 
+    bool primeira_interacao = true;
+    int cursor_pos = 0;
+    arquivo.seekg(ios::beg);
+    string nome_arquivo_id;
+    string nome_do_arquivo_atual;
+    
+
+    while (getline(arquivo, linha))
+    {
+            //Altera o nome do arquivo para a coluna que está sendo varrida
+        if(primeira_interacao  == true){
+        
+            nome_do_arquivo_atual = linha;
+            // função nomear arquivo novo aberto
+            nome_arquivo_id = linha;
+            nome_arquivo_id.append("_id.csv");
+            ID_Arquivo.open(nome_arquivo_id, ios::in | ios::app);
+
+
+            ID_Arquivo << linha << endl;
+            
+            primeira_interacao = false;
+            continue;
+        }
+
+        
+
+        //Cria e adiciona um dicionario para o arquivo
+        if(dicionario[linha] == false || dicionario.empty()){
+            dicionario[linha] = id;
+            id++;
+        }
+        // será transformado em função
+        // converte inteiro para string para utilizar na escrita
+        ostringstream aux;
+        aux << dicionario[linha];
+        string id_char = aux.str();
+        id_char.append(",");
+    
+        ID_Arquivo << id_char << endl;
+       
+    }
+    // apaga arquivo de coluna;
+    nome_do_arquivo_atual.append(".csv");
+    arquivo.close();
+    remove(nome_do_arquivo_atual.c_str());
 }
